12707cff568552eebe41ebe2e17a4f420820a537
tharunkumardeveloper <142856777+tharunkumardeveloper@users.noreply.github.com>
2025-09-17 11:30:51 +0530

    Update Tharun.py

--- Tharun.py
+++ Tharun.py
@@ -1,8 +1,8 @@
-print("Hello World");
-print("Adding 2 two Numbers");
-int a;
-int b;
+print("Hello World")
+print("Adding 2 two Numbers")
+int a
+int b
 a=input("Enter First Number: ")
 b=input("Enter Second Number: ")
-int c = a + b;
+int c = a + b
 print("Thus the result is ",c)
